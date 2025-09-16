4300e087964181dafa22c821ebd87fe24f968519
Bennett Lin <bennettslin@gmail.com>
2025-09-16 14:54:36 -0700

    Remove option store.

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import { getOptionReducer } from './option/reducer'
 import { getPageReducer } from './page/reducer'
-import { OPTION_STORE, PAGE_STORE } from '../constants/store'
+import { PAGE_STORE } from '../constants/store'
 
 export const getReducers = ({ initialPage }) => combineReducers({
-    [OPTION_STORE]: getOptionReducer(),
     [PAGE_STORE]: getPageReducer({ initialPage }),
 })
